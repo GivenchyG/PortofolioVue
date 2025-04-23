461ee18909fdb38af7c0ff860b09634b1a53f69a
GivenchyG <givenchy.html@gmail.com>
2025-04-23 15:21:15 +0200

    #3 Darkmode

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import DarkMode from '@/components/DarkMode.vue';
 </script>
 
 <template>
   <div></div>
-
-  <!-- <RouterView /> -->
+<DarkMode />
+<RouterView />
 </template>
 
 <style scoped></style>
---
+++ src/components/DarkMode.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { useDark, useToggle } from "@vueuse/core";
+
+const isDark = useDark();
+const toggleDark = useToggle(isDark);
+</script>
+
+<template>
+  <p>Dark theme: {{isDark}}</p>
+
+  <button @click="toggleDark()">
+    Toggle Color Mode
+  </button>
+</template>
+
+<style>
+.dark {
+  background: #16171d; 
+  color: #fff;
+}
+</style>
