1ff85d3154a2df5c7e8e538dc60d1782ce07fa1a
GivenchyG <givenchy.html@gmail.com>
2025-04-23 15:02:14 +0200

    #2 Tailwind

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import './assets/main.css'
+import '@/style.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
@@ -7,6 +7,7 @@ import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
+
 
 app.use(createPinia())
 app.use(router)
